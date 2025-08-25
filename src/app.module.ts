8b41406e78c3428b4bc076c8fbd913fd253214ae
Thiago Valdez <thiago@kaitsoft.com.ar>
2025-08-25 14:56:16 -0300

    SCRUM-27 Desarrollar la lógica de venta de stock

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { CategoriasModule } from './categorias/categorias.module';
 import { ProductosStockModule } from './productos-stock/productos-stock.module';
 import { FacturasModule } from './facturas/facturas.module';
 import { RegistrosModule } from './registros/registros.module';
+import { VentasModule } from './ventas/ventas.module';
 @Module({
   imports: [UsuariosModule,
     ConfigModule.forRoot(),
@@ -38,7 +39,8 @@ import { RegistrosModule } from './registros/registros.module';
     CategoriasModule,
     ProductosStockModule,
     FacturasModule,
-    RegistrosModule
+    RegistrosModule,
+    VentasModule
   ],
   controllers: [],
   providers: [BaseService],
